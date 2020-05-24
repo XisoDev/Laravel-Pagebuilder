6e1e4a1d5d5a6c6fc0a032f1dbc32648ad2c41f6
xisoDev <xiso@xiso.co.kr>
2020-05-25 03:46:42 +0900

    기본포트 수정

--- config/pagebuilder.php
+++ config/pagebuilder.php
@@ -29,6 +29,7 @@ return [
         'database' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST'),
+            'port'      => env('DB_PORT'),
             'database'  => env('DB_DATABASE'),
             'username'  => env('DB_USERNAME'),
             'password'  => env('DB_PASSWORD'),
